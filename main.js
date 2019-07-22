aff0206ec303921db193e9be41289b373091a050
Colin Jaffe <balloonasaurus@gmail.com>
2019-07-22 11:27:00 -0400

    Adds additional challenges and rmeoves extraneous comments.

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ const nasafyButton = document.querySelector('.blastoff');
 //When it's clicked, launch `biggify`.
 nasafyButton.onclick = nasafy;
 
-// TODO: Find the crazyify string button on our page.
+// TODO: Find the crazify string button on our page.
 const crazifyStringButton = document.querySelector('.crazy');
-//When it's clicked, launch `stringify`.
-crazifyStringButton.onclick = crazyify;
+//When it's clicked, launch `crazify`.
+crazifyStringButton.onclick = crazify;
 
 // TODO: Find the reverse string button on our page.
 const reverseStringButton = document.querySelector('.reverse');
@@ -22,6 +22,11 @@ reverseStringButton.onclick = reverse;
 const luckyNumbersButton = document.querySelector('.lucky');
 //When it's clicked, launch `luckify`.
 luckyNumbersButton.onclick = luckify;
+
+// TODO: Find the lucky numbers button on our page.
+const titleCaseButton = document.querySelector('.title');
+//When it's clicked, launch `titleify`.
+titleCaseButton.onclick = titleify;
 
 
 function biggify(event) {
@@ -55,7 +60,8 @@ function biggify(event) {
   Essentially, you can use `printValue` the same way you would `console.log`,
   only it will write to the DOM instead of to the console.
   */
- 
+
+
   const biggified = userInput + 9000;
   printValue(biggified);
 
@@ -68,7 +74,7 @@ function biggify(event) {
     experience to ask them to delete anything they've inputted previously before
     inputting something else.
   */
-   
+  
   inputBox.value = '';
 }
 
@@ -77,9 +83,9 @@ function nasafy(event) {
   event.preventDefault();
 
   /*
-  TODO: Call the function `clearList`, which we have defined for you. You
-  don't need to put anything in the parentheses to tell it what to clear, just
-  call it--it knows what to do!
+    TODO: Call the function `clearList`, which we have defined for you. You
+    don't need to put anything in the parentheses to tell it what to clear, just
+    call it--it knows what to do!
   */
 
   clearList();
@@ -90,17 +96,13 @@ function nasafy(event) {
   
   // TODO: Use the variable you saved in the above step to dive deeper into the
   // object and find the value the user has added there. Save it to a variable.
-  
   const userInput = inputBox.value;
 
   /*
-    TODO: Print every value from the user's number down to (and including) 1. Then print "Blastoff!".
+    TODO: Print every value from the user's number down to (and including) the number 1. Then print "Blastoff!".
     
-    Use the pre-defined `printValue` function below to print out the result,
+    Use the pre-defined `printValue` function below to print out each line,
     giving it the value you want to print.
-    
-    Essentially, you can use `printValue` the same way you would `console.log`,
-    only it will write to the DOM instead of to the console.
   */
 
   let current = userInput;
@@ -113,18 +115,14 @@ function nasafy(event) {
   printValue('Blastoff!');
 
   /*
-  Stretch goal TODO:
-  Using the variable we saved above for the input box, clear its value.
-  
-  This is a very useful feature, as it's an annoying and unexpected user
-  experience to ask them to delete anything they've inputted previously before
-  inputting something else.
+    Stretch goal TODO:
+    Using the variable we saved above for the input box, clear its value.
   */
   
   inputBox.value = '';
 }
 
-function crazyify(event) {
+function crazify(event) {
   // Make sure the page isn't reloaded when the user submits the form.
   event.preventDefault();
   
@@ -134,8 +132,8 @@ function crazyify(event) {
   call it--it knows what to do!
   */
 
- clearList();
- 
+  clearList();
+  
  // TODO: Find the input box the user is typing in and put the DOM node in a variable.
   // Make sure you're using the right selector!t
   const inputBox = document.querySelector('.user-input');
@@ -152,9 +150,6 @@ function crazyify(event) {
     
     Use the pre-defined `printValue` function below to print out result,
     giving it the value you want to print.
-    
-    Essentially, you can use `printValue` the same way you would `console.log`,
-    only it will write to the DOM instead of to the console.
   */
   
   let crazified = '';
@@ -248,9 +243,6 @@ function reverse(event) {
     
     Use the pre-defined `printValue` function below to print out the result,
     giving it the value you want to print.
-    
-    Essentially, you can use `printValue` the same way you would `console.log`,
-    only it will write to the DOM instead of to the console.
 
     Guidelines: 
     * You will find solutions on the internet that involve using `split` to make the string into an array. This is _not_ the correct solution for this exercise! Besides being incredibly inefficient (although this is a matter of principle more than app speed), this solution would teach you nothing about how to iterate over strings--and probably nothing about iterating over arrays either, since we don't have the foundational knowledge of arrays yet.
@@ -273,10 +265,6 @@ function reverse(event) {
   /*
     Stretch goal TODO:
     Using the variable we saved above for the input box, clear its value.
-
-    This is a very useful feature, as it's an annoying and unexpected user
-    experience to ask them to delete anything they've inputted previously before
-    inputting something else.
   */
 
   inputBox.value = '';
@@ -310,41 +298,165 @@ function luckify(event) {
 
     Use the pre-defined `printValue` function below to print out each line,
     giving it the value you want to print.
-    
-    Essentially, you can use `printValue` the same way you would `console.log`,
-    only it will write to the DOM instead of to the console.
   */
 
   let count = 0;
 
-  let numbers = '';
-
-  while (count < 5 && count < userInput) {
+  while (count < 5) {
     const random = Math.ceil(Math.random() * userInput);
-    const randomString = random.toString().padStart(userInput.length, 0)
-    if (!(numbers.includes(randomString))) {
-      numbers = numbers +  '|' + randomString;
-      printValue(random);
-      count = count + 1;
-    }
+    printValue(random);
+
+    count = count + 1;
   }
 
   /*
     Stretch Goal TODOS:
 
     * Make sure that you don't give them the same lucky number more than once. There are a few possible solutions to this, but don't use arrays! We'll get there soon. NOTE: the luckify stretch goals are particularly hard ones, feel free to come back after doing other stretch goals first.
-    * If you're preventing them getting the same number, but they type in, say, a 3, your browser will enter an infinite loop, as you can't get 5 numbers from 1-3 without repeating a number. But you _could_ have it simply exit the loop earlier if the user entered a lower number than 5.
-    * If you get a lucky number of 15, _some_ solutions to the above will prevent a 1 from coming afterwards, as it will consider the 1 digit in 15 as an instance of 1 having already occurred. (Feel free to test this with 19 as an input, as you'll get plenty of numbers with 1 digits in them, and you should still get a 1 after a number in the teens fairly often if your solution DOESN'T have this issue.) One solution to this involves the string method `padStart`... but feel free to find a different way!
   */
+  
+  // let done = false;
+  // let num1 = 0;
+  // let num2 = 0;
+  // let num3 = 0;
+  // let num4 = 0;
+  // let num5 = 0;
+
+  // while (!done) {
+  //   const random = Math.ceil(Math.random() * userInput);
+  //   if (!num1) {
+  //     num1 = random;
+  //   } else if (!num2) {
+  //     if (random !== num1) {
+  //       num2 = random;
+  //     }
+  //   } else if (!num3) {
+  //     if (random !== num1 && random !== num2) {
+  //       num3 = random;
+  //     }
+  //   } else if (!num4) {
+  //     if (random !== num1 && random !== num2 && random !== num3) {
+  //       num4 = random;
+  //     }
+  //   } else if (!num5) {
+  //     if (random !== num1 && random !== num2 && random !== num3 && random !== num4) {
+  //       num5 = random;
+  //     }
+  //   } else {
+  //     done = true;
+  //   }
+  // }
+
+  // printValue(num1);
+  // printValue(num2);
+  // printValue(num3);
+  // printValue(num4);
+  // printValue(num5);
+
+  // let count = 0;
+  // let numbers = '';
+
+  // while (count < 5) {
+  //   const random = Math.ceil(Math.random() * userInput);
+  //   if (!(numbers.includes(random))) {
+  //     numbers = numbers +  '|' + random;
+  //     printValue(random);
+  //     count = count + 1;
+  //   }
+  // }
+
+  /*
+    * If you're preventing them getting the same number, but they type in, say, a 3, your browser will enter an infinite loop, as you can't get 5 numbers from 1-3 without repeating a number. But you _could_ have it simply exit the loop earlier if the user entered a lower number than 5.
+  */
+
+  // let count = 0;
+  // let numbers = '';
+
+  // while (count < 5 && count < userInput) {
+  //   const random = Math.ceil(Math.random() * userInput);
+  //   if (!(numbers.includes(random))) {
+  //     numbers = numbers +  '|' + random;
+  //     printValue(random);
+  //     count = count + 1;
+  //   }
+  // }
+  /*
+    * If you get a lucky number of 15, _some_ solutions to the above will prevent a 1 from coming afterwards, as it will consider the 1 digit in 15 as an instance of 1 having already occurred. (Feel free to test this with 19 as an input, and see if a number with a 1 digit is ever followed by a 1.) One solution to this involves the string method `padStart`... but feel free to find a different way!
+  */
+
+  // let count = 0;
+  // let numbers = '';
+
+  // while (count < 5 && count < userInput) {
+  //   const random = Math.ceil(Math.random() * userInput);
+  //   const randomString = random.toString().padStart(userInput.length, 0)
+  //   if (!(numbers.includes(randomString))) {
+  //     numbers = numbers +  '|' + randomString;
+  //     printValue(random);
+  //     count = count + 1;
+  //   }
+  // }
 
 
   /*
     Stretch goal TODO:
     Using the variable we saved above for the input box, clear its value.
+  */
 
-    This is a very useful feature, as it's an annoying and unexpected user
-    experience to ask them to delete anything they've inputted previously before
-    inputting something else.
+  inputBox.value = '';
+}
+
+function titleify(event) {
+  // Make sure the page isn't reloaded when the user submits the form.
+  event.preventDefault();
+
+/*
+  TODO: Call the function `clearList`, which we have defined for you. You
+  don't need to put anything in the parentheses to tell it what to clear, just
+  call it--it knows what to do!
+*/
+
+  clearList();
+  
+  // TODO: Find the input box the user is typing in and put the DOM node in a variable.
+  // Make sure you're using the right selector!
+  const inputBox = document.querySelector('.user-input');
+  
+  // TODO: Use the variable you saved in the above step to dive deeper into the
+  // object and find the value the user has added there. Save it to a variable.
+
+  const userInput = inputBox.value;
+
+
+  /*
+    TODO: Print out the title case version of the string the user inputted.
+
+    Use the pre-defined `printValue` function below to print out the result,
+    giving it the value you want to print.
+
+    What's title case? It's the case used for the title of a book or movie. If it's the first letter of a word, you use a capital letter. Otherwise, use lower case.
+
+    True title case keeps lower case for any word that's an article or preposition, but we're _definitely_ not asking that of you!
+  */
+
+  let titleCase = '';
+  let i = 0;
+  while (i < userInput.length) {
+    if (i === 0 || userInput[i - 1] === ' ') {
+      titleCase = titleCase + userInput[i].toUpperCase();
+    } else {
+      titleCase = titleCase + userInput[i].toLowerCase();
+    }
+
+    i = i + 1;
+  }
+
+  printValue(titleCase);
+
+
+  /*
+    Stretch goal TODO:
+    Using the variable we saved above for the input box, clear its value.
   */
 
   inputBox.value = '';
